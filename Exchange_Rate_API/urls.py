2f59e84253bb4e61daca9a7657e442d782d6638f
Roman Shupak <romanshupak808@gmail.com>
2025-03-17 19:16:20 +0200

    Create main endpoints as required.

--- Exchange_Rate_API/urls.py
+++ Exchange_Rate_API/urls.py
@@ -14,9 +14,22 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+from rest_framework import routers
+from exchange.views import (
+    CurrencyExchangeViewSet,
+    RegisterView,
+    CurrencyHistoryView,
+    BalanceView
+)
+
+router = routers.DefaultRouter()
+router.register(r"currency", CurrencyExchangeViewSet, basename="currency")
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path("register/", RegisterView.as_view(), name="register"),
+    path("history/", CurrencyHistoryView.as_view(), name="history"),
+    path("balance/", BalanceView.as_view(), name="balance"),
+    path("", include(router.urls)),
+    path("user/", include("exchange.urls")),
 ]
