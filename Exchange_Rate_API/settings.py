a4bd88c1f362cc258419c8cea1a2ba2b278433a4
Roman Shupak <romanshupak808@gmail.com>
2025-03-17 12:22:34 +0200

    Create .env with exchange rate api and add it to settings.

--- Exchange_Rate_API/settings.py
+++ Exchange_Rate_API/settings.py
@@ -9,8 +9,14 @@ https://docs.djangoproject.com/en/5.1/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.1/ref/settings/
 """
-
+import os
+from dotenv import load_dotenv
 from pathlib import Path
+
+load_dotenv()
+
+EXCHANGE_RATE_API_KEY = os.getenv("EXCHANGE_RATE_API_KEY")
+EXCHANGE_RATE_API_URL = os.getenv("EXCHANGE_RATE_API_URL")
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
